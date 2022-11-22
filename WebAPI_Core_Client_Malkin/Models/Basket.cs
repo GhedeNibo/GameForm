bd55012dcb306687039580a1c3035eec159d3d37
student-222 <asrtemmashenkoff20158@gmail.com>
2022-11-22 14:52:26 +0300

    Added models for baskets table

---
+++ WebAPI_Core_Client_Malkin/Models/Basket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI_Core_Client_Malkin.Models
+{
+    class Basket
+    {
+        public int ID { get; set; }
+        public int GameId { get; set; }
+        public int Count { get; set; }
+        public int UserId { get; set; }
+    }
+}
